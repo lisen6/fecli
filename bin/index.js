557f6bedd55e7871ccbb34ee53122bfe3bb5ee2c
vayne1q <1789502923@qq.com>
2022-02-20 17:48:19 +0800

    模板改为拉取本地模板，支持dev开发与build打包，支持.env.[name]环境文件

--- bin/index.js
+++ bin/index.js
@@ -1,69 +1,55 @@
 #!/usr/bin/env node
 
-const {
-  program
-} = require("commander");
-const chalk = require("chalk");
-const figlet = require("figlet");
-const pkg = require("../package");
-const create = require("../lib/create");
-const {
-  startService
-} = require("../webpack/start");
+const { program } = require('commander')
+const chalk = require('chalk')
+const figlet = require('figlet')
+const pkg = require('../package')
 
-const {
-  buildService
-} = require('../webpack/build')
-
-// 拉取远程模板
 program
-  .command("init <name>")
-  .description("Generate a project from a remote template")
-  .option("-f, --force", "overwrite target directory if it exist")
+  .command('init <name>')
+  .description('Generate a project from a remote template')
+  .option('-f, --force', 'overwrite target directory if it exist')
   .action(async (name, options) => {
-    await create(name, options);
-  });
+    require('../utils/downloadTemplate').downloadTemplate(name, options)
+  })
 
-// 开启服务
 program
-  .command("dev")
-  .description("Start the project")
+  .command('dev')
+  .description('start the project')
   .action(() => {
-    startService();
-  });
+    require('../webpack/startService').startService()
+  })
 
-// 打包
 program
-  .command("build")
-  .description("build the project")
+  .command('build')
+  .description('build the project')
   .action(() => {
-    buildService();
-  });
+    require('../webpack/webpack.build').buildService()
+  })
 
 program
   .version(chalk.green(`${pkg.version}`))
-  .option("-d, --debug", "output extra debugging")
+  .option('-d, --debug', 'output extra debugging')
   // 监听 --help 执行
-  .on("--help", () => {
-
+  .on('--help', () => {
     // 打印脚手架logo
     console.log(
-      "\r\n" +
-      figlet.textSync("fecli", {
-        font: "Ghost",
-        horizontalLayout: "default",
-        verticalLayout: "default",
-        width: 80,
-        whitespaceBreak: true,
-      })
-    );
+      '\r\n' +
+        figlet.textSync('fecli', {
+          font: 'Ghost',
+          horizontalLayout: 'default',
+          verticalLayout: 'default',
+          width: 80,
+          whitespaceBreak: true,
+        }),
+    )
 
     // 新增说明信息
     console.log(
       `\r\nRun ${chalk.cyan(
-        `fecli <command> --help`
-      )} for detailed usage of given command\r\n`
-    );
-  });
+        `fecli <command> --help`,
+      )} for detailed usage of given command\r\n`,
+    )
+  })
 
-program.parse(process.argv);
+program.parse(process.argv)
